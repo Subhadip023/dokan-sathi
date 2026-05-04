aee3c89ea192345fc4df5c71a66f277f8ac20631
debashismidya <debashis.matainja@gmail.com>
2026-05-04 20:02:47 +0530

    server setup

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Support\Facades\URL;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -21,5 +21,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Vite::prefetch(concurrency: 3);
+        // Force HTTPS if not on local
+        if (config('app.env') !== 'local') {
+            URL::forceScheme('https');
+        }
     }
 }
